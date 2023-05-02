6e213d969bb77cbabfc40f002045746ed12f51b3
Christine Wong <jumiwong@Christines-MacBook-Air.local>
2023-05-01 20:56:09 -0700

    homework submission

--- first_js_homework.js
+++ first_js_homework.js
@@ -6,28 +6,105 @@ Some tools used to avoid writing repetitive codes are loop, function, descriptiv
 let is used for variables that are only available inside the block where they're defined, whereas
 var is used for variables throughout the entire function in which they're declared. */
 
-const a = 4;
-const b = 53;
-const c = 57;
-const d = 16;
-const e = 'Kelvin';
-const f = false;
-let g = 0;
-g = b + c;
-
-console.log(a<b)
 
 /* Section 2
+
+a < b
+c > d
+'Name' == 'Name'
+a < b < c
+a == a < d 
+a == a !== d 
+e !== 'Kevin'
+48 !== '48'
+f !== e 
+g == 110 
+
 I used 'let' because g needed to be assigned with more than one value. 
 If you use 'var', it would stay the same in this case. 
 However, using 'const' will give us a TypeError message because const values cannot have more than one value.
 Writing 10 = g will result in a SyntaxError: invalid left-hand side in assignment because 
 10 was not previously assigned with either let, var, or const.
+
+Bonus Challenge:
+a _ (b || f) _ !f && e _ c 
+console.log(a < (b || f) === !f && e !== c)
+*/
+
+/* Section 3
+The code is an infinite loop because there's no exit clause to stop it from running.
+The second code will give an error because the value of runProgram is set as constant, 
+therefore when there's another value introduced after console.log it negates the first value so it's not going to run.
+
+let letters = "A";
+// start a while loop that will run where letters is defined as "A", 
+let i = 0;
+while (i < 20) {
+    // loop will run as long as i is less than 20
+	letters += "A";
+    // letters is increased by the value of "A"
+	i++;
+    // increments the value of i by 1 and closes the while loop;
+   // the loop will continue to run until the while condition becomes false
+
+}
+
+console.log(letters);
+// print the value of letters */
+
+/*Expected result: print A 20 times 
+Actual result: printed A 21 times. 
+I'm guessing the reason why it printed 21 As is because by default 
+it will always print one A in the beginning regardless of 'i'.
 */
 
 
+/*Section 4
+
+The key differences between for loops and while loops is that 
+for loop is used for definite loops when the number of iteration is known, 
+whereas while loops are used for indefinite loops, and they run until 
+condition of the statement in the program becomes false.
+For loop is useful when you know the exact number of times the loop has to be
+repeated, where as while loop is used when the number of iteration is unknown.
 
 
-/* Section 3
-The code is an infinite loop because there's no exit clause to stop it from running.*/
+The first part of the control panel is: i =0 (initialization)
+The second part of the control panel is: i < 100 (condition)
+The third part of the control panel is: i++ (increment)
 
+Backlash \ is used to start an escape sequence, whereas forward slash /
+is used in place of "or". */
+
+/*for (let i = 999; i >= 0; i--){
+    console.log(i)
+} */
+
+/*for (let i = 1; i <= 10; i++){
+    console.log("The value of i is: " + (i) + " of 10")
+} */
+
+/*for (let i = 1; i <= 10; i++){
+    console.log(`The value of i is: ${i} of 10`)
+} */
+
+// ITERATION
+
+/*const StarWars = ["Han", "C3PO", "R2D2", "Luke", "Leia", "Anakin"];
+
+for (let i = 0; i < StarWars.length; i++){
+    console.log(StarWars[i])
+} */
+
+/*const StarWars = ["Han", "C3PO", "R2D2", "Luke", "Leia", "Anakin"];
+
+StarWars.forEach((value, index) => {
+    console.log(value, index)
+}) */
+
+/* Bonus Challenge
+const StarWars = ["Han", "C3PO", "R2D2", "Luke", "Leia", "Anakin"];
+
+for (let i = 0; i <5; i++){
+    if (i % 2 === 0) console.log(StarWars[i])
+} */
--- terminal_homework.bash
+++ terminal_homework.bash
@@ -1,17 +1,76 @@
-jumiwong@Christines-MacBook-Air ~ % cd desktop
-jumiwong@Christines-MacBook-Air desktop % mkdir first_homework
-jumiwong@Christines-MacBook-Air desktop % cd first_homework
-jumiwong@Christines-MacBook-Air first_homework % touch first_js_homework.js terminal_homework.bash
-jumiwong@Christines-MacBook-Air first_homework % node first_js_homework.js
-jumiwong@Christines-MacBook-Air first_homework % node first_js_homework.js
-4
-jumiwong@Christines-MacBook-Air first_homework % node first_js_homework.js
-53
-jumiwong@Christines-MacBook-Air first_homework % node first_js_homework.js
-57
-jumiwong@Christines-MacBook-Air first_homework % node first_js_homework.js
-16
-jumiwong@Christines-MacBook-Air first_homework % node first_js_homework.js
-Kelvin
-jumiwong@Christines-MacBook-Air first_homework % node first_js_homework.js
-false
+# Copy this text into your `.bash` homework file. Copy and paste the code you successfully used into terminal below each prompt
+
+# Open the **Terminal app**
+
+# Create a new directory on your desktop called `galaxy_far_far_away`and enter it
+jumiwong@Christines-MacBook-Air Desktop % mkdir galaxy_far_far_away
+# Create a directory called `death_star`
+jumiwong@Christines-MacBook-Air galaxy_far_far_away % mkdir death_star
+# and make the following files inside of it:
+# darth_vader.txt
+# princess_leia.txt
+# storm_trooper.txt
+jumiwong@Christines-MacBook-Air death_star % touch darth_vader.txt princess_leia.txt storm_trooper.txt
+# In `galaxy_far_far_away`, make a directory named `tatooine`
+jumiwong@Christines-MacBook-Air galaxy_far_far_away % mkdir tatooine
+# and create the following files in it:
+# luke.txt
+# ben_kenobi.txt
+jumiwong@Christines-MacBook-Air tatooine % touch luke.txt ben_kenobi.txt
+# Inside of `tatooine` make a directory called `millenium_falcon`
+# and in it create:
+# han_solo.txt
+# chewbaca.txt
+jumiwong@Christines-MacBook-Air tatooine % mkdir millenium_falcon
+jumiwong@Christines-MacBook-Air tatooine % cd millenium_falcon
+jumiwong@Christines-MacBook-Air millenium_falcon % touch han_solo.txt chewbaca.txt
+# Rename `ben_kenobi.txt` to `obi_wan.txt
+jumiwong@Christines-MacBook-Air tatooine % mv ben_kenobi.txt obi_wan.txt
+# Copy `storm_trooper.txt` from `death_star` to `tatooine`
+cp storm_trooper.txt ../tatooine
+# Move `luke.txt` and `obi_wan.txt` to the `millenium_falcon`
+mv luke.txt obi_wan.txt millenium_falcon
+# Move `millenium_falcon` out of `tatooine` and into `galaxy_far_far_away`
+mv millenium_falcon ..
+# Move `millenium_falcon` into `death_star`
+jumiwong@Christines-MacBook-Air galaxy_far_far_away % mv millenium_falcon death_star
+# Move `princess_leia.txt` into the `millenium_falcon`
+jumiwong@Christines-MacBook-Air death_star % mv princess_leia.txt millenium_falcon
+# Delete `obi_wan.txt`
+jumiwong@Christines-MacBook-Air millenium_falcon % rm obi_wan.txt
+# In `galaxy_far_far_away`, make a directory called `yavin_4`
+jumiwong@Christines-MacBook-Air galaxy_far_far_away % mkdir yavin_4 
+# Move the `millenium_falcon` out of the `death_star` and into `yavin_4`
+jumiwong@Christines-MacBook-Air death_star % mv millenium_falcon ../yavin_4
+# Make a directory in `yavin_4` called `x_wing`
+jumiwong@Christines-MacBook-Air yavin_4 % mkdir x_wing
+# Move `princess_leia.txt` to `yavin_4` and `luke.txt` to `x_wing`
+jumiwong@Christines-MacBook-Air millenium_falcon % mv princess_leia.txt ..
+jumiwong@Christines-MacBook-Air millenium_falcon % mv princess_leia.txt ../yavin_4
+# Move the `millenium_falcon` and `x_wing` out of `yavin_4` and into `galaxy_far_far_away`
+jumiwong@Christines-MacBook-Air yavin_4 % mv millenium_falcon x_wing ..
+# In `death_star`, create directories for `tie_fighter_1`, `tie_fighter_2` and `tie_fighter_3`
+jumiwong@Christines-MacBook-Air death_star % mkdir tie_fighter_1 tie_fighter_2 tie_fighter_3
+# Move `darth_vader.txt` into `tie_fighter_1`
+jumiwong@Christines-MacBook-Air death_star % mv darth_vader.txt tie_fighter_1
+# Make a copy of `storm_trooper.txt` in both `tie_fighter_2` and `tie_fighter_3`
+jumiwong@Christines-MacBook-Air death_star % cp storm_trooper.txt tie_fighter_2
+jumiwong@Christines-MacBook-Air death_star % cp storm_trooper.txt tie_fighter_3
+# Move all of the `tie_fighters` out of the `death_star` and into `galaxy_far_far_away`
+jumiwong@Christines-MacBook-Air death_star % mv tie_fighter_1 tie_fighter_2 tie_fighter_3 ..
+# Be careful with this command - cannot undo!
+
+# Make sure you delete the right thing, or you could accidentally delete the contents of your computer (it has happened).
+
+# This command will typically not ask you if you really want to delete. It will just delete so doublecheck you are deleting exactly what you mean to delete
+
+# Remove `tie_fighters` 2 and 3.
+jumiwong@Christines-MacBook-Air galaxy_far_far_away % rm -r tie_fighter_2 tie_fighter_3
+# Touch a file in "**x_wing**" called "**the_force.txt**".
+jumiwong@Christines-MacBook-Air x_wing % touch the_force.txt
+# Destroy the "**death_star**" and anyone inside of it.
+jumiwong@Christines-MacBook-Air galaxy_far_far_away % rm -r death_star
+# Return "**x_wing**" and the "**millenium_falcon**" to "**yavin_4**".
+jumiwong@Christines-MacBook-Air galaxy_far_far_away % mv x_wing millenium_falcon yavin_4
+# Celebrate!
+YAY
